d557279e265eac2d0d2860fab125f37c99079f85
LauraA <Laura.Acquaviva@nttdata.com>
2023-01-10 17:47:31 +0100

    Commit calcolatrice

---
+++ calcolatrice/src/main/java/it/corso/calcolatrice/Calcolatrice.java
@@ -0,0 +1,30 @@
+package it.corso.calcolatrice;
+
+
+public class Calcolatrice {
+
+	public Calcolatrice() {}
+	
+	public double somma(double a, double b) {
+		return a + b;
+	}
+	
+	public double sottrazione(double a, double b) {
+		return a - b;
+	}
+	
+	public double divisione(double a, double b) {
+		return a / b;
+	}
+	
+	public double potenza(double a, double b) {
+		return Math.pow(a, b);
+	}
+
+	public double media(double a, double b) {
+		return (a + b)/2;
+	}
+	
+	
+	
+}
---
+++ calcolatrice/src/main/java/it/corso/calcolatrice/Main.java
@@ -0,0 +1,22 @@
+package it.corso.calcolatrice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+			Calcolatrice calc = new Calcolatrice();
+			double a = 10;
+			double b = 5;
+			System.out.println("Numeri selezionati: " + a + " e " + b);
+			System.out.println("Somma: " + calc.somma(a, b));
+			System.out.println("Sottrazione: " + calc.sottrazione(a, b));
+			System.out.println("Divisione: " + calc.divisione(a, b));
+			System.out.println("Potenza: " + calc.potenza(a, b));
+			System.out.println("Media: " + calc.media(a, b));
+		
+		
+
+	}
+
+}
