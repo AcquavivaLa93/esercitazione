a119e5fcb8fba501209242316e1fec9c2e864ed6
LauraA <Laura.Acquaviva@nttdata.com>
2023-01-11 10:49:26 +0100

    Calcolatrice terzo commit

---
+++ calcolatrice/src/test/java/it/corso/junitTest/calcolatriceTest.java
@@ -0,0 +1,23 @@
+package it.corso.junitTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import it.corso.calcolatrice.Calcolatrice;
+
+public class calcolatriceTest {
+
+	@Test
+	void createTest() {
+		Calcolatrice calc = new Calcolatrice();
+		double a = 10;
+		double b = 5;
+		
+		assertEquals(calc.somma(a, b), a+b);
+		assertEquals(calc.sottrazione(a, b), a-b);
+		assertEquals(calc.potenza(a, b), Math.pow(a, b));
+		assertEquals(calc.media(a, b), (a+b)/2);
+		assertEquals(calc.divisione(a, b), a/b);
+	}
+}
---
+++ calcolatrice/src/test/java/it/corso/junitTest/costruttoreTest.java
@@ -0,0 +1,16 @@
+package it.corso.junitTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import it.corso.calcolatrice.Calcolatrice;
+
+public class costruttoreTest {
+
+	@Test
+	void createTest() {
+		Calcolatrice cal = new Calcolatrice();
+		assertEquals(Calcolatrice.class,cal.getClass());
+	}
+}
